47ac00a43e891188372b0e16a1b838c2656eadc9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-17 23:56:50 +0100

    Update src/data/interface.py: Re-writing

--- src/data/interface.py
+++ src/data/interface.py
@@ -4,9 +4,6 @@ import typing
 
 import pandas as pd
 
-import src.data.source
-import src.data.tags
-import src.data.specimens
 import src.data.splittings
 
 
@@ -15,10 +12,13 @@ class Interface:
     Interface
     """
 
-    def __init__(self):
+    def __init__(self, frame: pd.DataFrame):
         """
-        Constructor
+
+        :param frame:
         """
+
+        self.__frame = frame
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -27,59 +27,17 @@ class Interface:
 
         self.__logger = logging.getLogger(__name__)
 
-    def __raw(self) -> pd.DataFrame:
-        """
-        The raw data
-
-        :return:
-        """
-
-        data: pd.DataFrame = src.data.source.Source().exc()
-        self.__logger.info(data)
-
-        return data
-
-    def __tags(self, data: pd.DataFrame) -> typing.Tuple[pd.DataFrame, dict, dict]:
-        """
-        The viable tags, and the corresponding tags enumerator & archetype
-
-        :param data:
-        :return:
-        """
-
-        elements, enumerator, archetype = src.data.tags.Tags(data=data).exc()
-        self.__logger.info(elements)
-        self.__logger.info(enumerator)
-        self.__logger.info(archetype)
-
-        return elements, enumerator, archetype
-
-    def exc(self):
-        """
-
-        :return:
-        """
-
-        # The raw data
-        data = self.__raw()
-
-        # The viable tags, etc.
-        elements, enumerator, archetype = self.__tags(data=data)
-
-        # The viable data instances vis-à-vis viable tags
-        data: pd.DataFrame = data.copy().loc[data['category'].isin(values=elements['category'].unique()), :]
-        self.__logger.info(data.head())
-        data.info()
-
-        # Hence, the expected structure.  Within the preceding dataframe each distinct sentence
-        # is split across rows; a word per row, in order.  The Specimen class re-constructs the
-        # original sentences.
-        frame: pd.DataFrame = src.data.specimens.Specimens(data=data).exc()
-        self.__logger.info(frame.head())
-        frame.info()
+    def __splits(self):
 
         training, validating, testing = src.data.splittings.Splittings(
-            frame=frame).exc()
+            frame=self.__frame).exc()
         training.info()
         validating.info()
         testing.info()
+
+
+    def get_datasets(self):
+        pass
+
+    def get_rays(self):
+        pass
