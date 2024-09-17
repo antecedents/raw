f05b198af1746b345d1a05d4317ea00d4c521660
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-17 21:59:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -18,12 +18,5 @@ class Config:
         # Addressing category imbalance ...
         self.minimum_category_frequency = 1000
 
-        # Splitting fractions:
-        # * 80% of the data for training
-        # * A tenth of the remaining 20% for testing, i.e., 18%
-        #   for validating, 2% for testing
-        self.fraction = 0.8
-        self.aside = 0.1
-
         # Seed: All cases
         self.seed = 5
