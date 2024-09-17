6edab47e9366c04969e77224c8f0d395e5659907
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-17 21:45:34 +0100

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,6 +1,30 @@
 """Module interface.py"""
+import logging
+
+import pandas as pd
+
+import src.data.source
+
 
 class Interface:
 
     def __init__(self):
-        pass
+        """
+        Constructor
+        """
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        data: pd.DataFrame = src.data.source.Source().exc()
+        self.__logger.info(data.head())
