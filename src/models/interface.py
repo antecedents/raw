f1809b5621d8185bcaa1851cbfc9b722e41e2ad1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-18 16:20:25 +0100

    Update src/models/interface.py

---
+++ src/models/interface.py
@@ -0,0 +1,42 @@
+import logging
+
+import datasets
+import pandas as pd
+
+import src.data.interface
+
+
+class Interface:
+    """
+    Interface: Models
+    """
+
+    def __init__(self, specimens: pd.DataFrame):
+        """
+
+        :param specimens:
+        """
+
+        self.__specimens: pd.DataFrame = specimens
+
+        # An instance for retrieving the train/validate/test splits of the data.  The methods
+        # deliver datasets.DatasetDict or dict[str, ray.data.dataset.MaterializedDataset]
+        self.__api = src.data.interface.Interface(data=self.__specimens)
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        data: datasets.DatasetDict = self.__api.get_datasets()
+
+        self.__logger.info(data.keys())
+
