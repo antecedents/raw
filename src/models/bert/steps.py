4b0412ea7a3ab506d91bbb692d2af62fb7b32bdd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-18 23:41:14 +0100

    Update src/models/bert/steps.py

--- src/models/bert/steps.py
+++ src/models/bert/steps.py
@@ -1,9 +1,33 @@
 
+import src.elements.frames as fa
+import src.elements.variable as vr
+
+import src.models.bert.dataset as dt
+import src.models.bert.tokenizer
+
+
 class Steps:
 
-    def __init__(self):
-        pass
+    def __init__(self, splittings: fa.Frames, enumerator: dict, archetype: dict):
+
+        self.__splittings = splittings
+        self.__enumerator = enumerator
+        self.__archetype = archetype
+
+        self.__variable = vr.Variable(
+            N_TRAIN=self.__splittings.training.shape[0], N_VALID=self.__splittings.validating.shape[0],
+            N_TEST=self.__splittings.testing.shape[0])
+
+        self.__tokenizer = src.models.bert.tokenizer.Tokenizer()
 
 
     def exc(self):
-        pass
+
+        training = dt.Dataset(frame=self.__splittings.training, variable=self.__variable,
+                              enumerator=self.__enumerator, tokenizer=self.__tokenizer)
+        validating = dt.Dataset(frame=self.__splittings.validating, variable=self.__variable,
+                                enumerator=self.__enumerator, tokenizer=self.__tokenizer)
+        testing = dt.Dataset(frame=self.__splittings.testing, variable=self.__variable,
+                             enumerator=self.__enumerator, tokenizer=self.__tokenizer)
+
+        training
