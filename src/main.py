62771473da1d656a7e875be1d19c0a8bad211451
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-19 17:42:51 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 
-import pandas as pd
+import ray
 import torch
 
 
@@ -18,6 +18,9 @@ def main():
     # Device  Selection: Setting a graphics processing unit as the default device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     logger.info(msg=device)
+
+    # Ray
+    ray.init(dashboard_host='172.17.0.2', dashboard_port=8265)
 
     # The raw data
     data = src.data.source.Source().exc()
@@ -46,6 +49,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Activate graphics processing units
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
+    os.environ['TOKENIZERS_PARALLELISM']='true'
+    os.environ['RAY_USAGE_STATS_ENABLED']='0'
 
     # Classes
     import src.data.encodings
