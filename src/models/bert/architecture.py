3d7410d67f69d496150ca3fd3dc33c0aa2caf29c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-19 23:58:21 +0100

    Update src/models/bert/architecture.py: In Progress

--- src/models/bert/architecture.py
+++ src/models/bert/architecture.py
@@ -1,5 +1,7 @@
 import logging
 
+import ray.train
+
 
 class Architecture:
 
@@ -10,3 +12,7 @@ class Architecture:
     def exc(config: dict):
 
         logging.info(config)
+
+        # Data & Tokens
+        training = ray.train.get_dataset_shard("train")
+        evaluating = ray.train.get_dataset_shard("eval")
