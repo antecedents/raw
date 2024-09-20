a611d4762d6c507beb18d4abece258bbe6fa1a58
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 17:03:48 +0100

    Update src/models/bert/architecture.py: In Progress

--- src/models/bert/architecture.py
+++ src/models/bert/architecture.py
@@ -1,9 +1,12 @@
 import logging
 
 import ray.train
+import transformers
 
 import src.models.bert.tokenizer
 import src.models.bert.special
+import src.elements.variable as vr
+import src.models.bert.parameters
 
 
 class Architecture:
@@ -16,6 +19,14 @@ class Architecture:
 
         logging.info(config)
 
+        # Via config
+        variable: vr.Variable = config.get('variable')
+        max_steps_per_epoch: int = (
+                variable.N_TRAIN // (variable.TRAIN_BATCH_SIZE * variable.N_GPU))
+
+        # Parameters
+        parameters = src.models.bert.parameters.Parameters()
+
         # Tokenizer
         tokenizer = src.models.bert.tokenizer.Tokenizer().exc()
 
@@ -23,11 +34,24 @@ class Architecture:
         special = src.models.bert.special.Special(
             variable=config.get('variable'), enumerator=config.get('enumerator'), tokenizer=tokenizer)
 
-        # Data & Tokens
         training = ray.train.get_dataset_shard('train')
         evaluating = ray.train.get_dataset_shard('eval')
 
         training_ = training.iter_torch_batches(
-            batch_size=config.get('train_batch_size'), collate_fn=special.exc)
+            batch_size=variable.TRAIN_BATCH_SIZE, collate_fn=special.exc)
         evaluating_ = evaluating.iter_torch_batches(
-            batch_size=config.get('valid_batch_size'), collate_fn=special.exc)
+            batch_size=variable.VALID_BATCH_SIZE, collate_fn=special.exc)
+
+        # Arguments
+        transformers.TrainingArguments(
+            output_dir=parameters.MODEL_OUTPUT_DIRECTORY,
+            eval_strategy='epoch', save_strategy='epoch',
+            learning_rate=config.get('learning_rate'),
+            weight_decay=config.get('weight_decay'),
+            per_device_train_batch_size=variable.TRAIN_BATCH_SIZE,
+            per_device_eval_batch_size=variable.VALID_BATCH_SIZE,
+            num_train_epochs=variable.EPOCHS,
+            max_steps=max_steps_per_epoch * variable.EPOCHS,
+            warmup_steps=0,
+            no_cuda=False
+        )
