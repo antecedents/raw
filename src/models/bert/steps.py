c9e7ae57abbea635a17f6225106d84d48dd68f4a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 14:14:09 +0100

    Update src/models/bert/steps.py

--- src/models/bert/steps.py
+++ src/models/bert/steps.py
@@ -3,9 +3,7 @@ import logging
 
 import ray.data.dataset
 
-import src.elements.frames as fr
 import src.elements.variable as vr
-import src.data.datatypes
 
 
 class Steps:
@@ -13,21 +11,21 @@ class Steps:
     Class Steps
     """
 
-    def __init__(self, splittings: fr.Frames, enumerator: dict, archetype: dict):
+    def __init__(self, data: dict[str, ray.data.dataset.MaterializedDataset], enumerator: dict, archetype: dict):
         """
 
-        :param splittings:
+        :param data:
         :param enumerator:
         :param archetype:
         """
 
-        self.__splittings = splittings
+        self.__data = data
         self.__enumerator = enumerator
         self.__archetype = archetype
 
         self.__variable = vr.Variable(
-            N_TRAIN=self.__splittings.training.shape[0], N_VALID=self.__splittings.validating.shape[0],
-            N_TEST=self.__splittings.testing.shape[0])
+            N_TRAIN=self.__data['train'].count(), N_VALID=self.__data['validate'].count(),
+            N_TEST=self.__data['test'].count())
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -41,9 +39,5 @@ class Steps:
         :return:
         """
 
-        # Possible -> Transfer to src/models/interface.py
-        data: dict[str, ray.data.dataset.MaterializedDataset] = src.data.datatypes.Datatypes(
-            splittings=self.__splittings).get_rays()
-
-        self.__logger.info(data)
-        self.__logger.info(data['train'].take(1))
+        self.__logger.info(self.__data)
+        self.__logger.info(self.__data['train'].take(1))
