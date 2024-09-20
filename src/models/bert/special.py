388094a6441b737ab86895c3d3d2c0127aa02f7c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 12:33:37 +0100

    Update src/models/bert/special.py

--- src/models/bert/special.py
+++ src/models/bert/special.py
@@ -1,5 +1,57 @@
 
+import numpy as np
+
+import transformers
+import torch
+
+import src.elements.variable as vr
+
+
 class Special:
 
-    def __init__(self):
-        pass
+    def __init__(self, variable: vr.Variable, enumerator: dict,
+                 tokenizer: transformers.tokenization_utils_base):
+        """
+
+        :param variable:
+        :param enumerator:
+        :param tokenizer:
+        """
+
+        self.__variable = variable
+        self.__enumerator = enumerator
+        self.__tokenizer = tokenizer
+
+    def exc(self, node: dict):
+        """
+
+        :param node:
+        :return:
+        """
+
+        # A sentence's words, and the tokenization of words
+        words: list[str] = node['sentence'].strip().split()
+        encoding: dict = self.__tokenizer(words, padding='max_length', truncation=True,
+                                          is_split_into_words=True,
+                                          max_length=self.__variable.MAX_LENGTH,
+                                          return_offsets_mapping=True)
+
+        # placeholder array of labels for the encoding dict
+        ela: np.ndarray = np.ones(shape=self.__variable.MAX_LENGTH, dtype=int) * -100
+
+        # The corresponding tags of a sentence's words, and the code of each tag
+        tags: list[str] = node['tagstr'].split(',')
+        labels = [self.__enumerator[tag] for tag in tags]
+
+        # Herein, per iteration, cf. offset pairings.  There
+        # are <max_length> iterations/offset pairings.
+        # (maximum number of tokens, 2)
+        limit = len(labels)
+        for iteration, mapping in enumerate(encoding['offset_mapping']):
+            if mapping[0] == 0 and mapping[1] != 0 and iteration < limit:
+                ela[iteration] = labels[iteration]
+
+        encoding['labels'] = ela
+        item = {key: torch.as_tensor(value) for key, value in encoding.items()}
+
+        return item
