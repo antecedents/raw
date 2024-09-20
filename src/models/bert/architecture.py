bad977630129f454fdfaebd48c4311c928012ddd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 14:01:33 +0100

    Update src/models/bert/architecture.py: In Progress

--- src/models/bert/architecture.py
+++ src/models/bert/architecture.py
@@ -2,6 +2,9 @@ import logging
 
 import ray.train
 
+import src.models.bert.tokenizer
+import src.models.bert.special
+
 
 class Architecture:
 
@@ -13,6 +16,18 @@ class Architecture:
 
         logging.info(config)
 
+        # Tokenizer
+        tokenizer = src.models.bert.tokenizer.Tokenizer().exc()
+
+        # Special
+        special = src.models.bert.special.Special(
+            variable=config.get('variable'), enumerator=config.get('enumerator'), tokenizer=tokenizer)
+
         # Data & Tokens
-        training = ray.train.get_dataset_shard("train")
-        evaluating = ray.train.get_dataset_shard("eval")
+        training = ray.train.get_dataset_shard('train')
+        evaluating = ray.train.get_dataset_shard('eval')
+
+        training_ = training.iter_torch_batches(
+            batch_size=config.get('train_batch_size'), collate_fn=special.exc)
+        evaluating_ = evaluating.iter_torch_batches(
+            batch_size=config.get('valid_batch_size'), collate_fn=special.exc)
