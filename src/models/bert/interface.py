7174bf826d59b6a5769c55c72379d6f51cab9c3f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 16:51:10 +0100

    Update src/models/bert/interface.py

--- src/models/bert/interface.py
+++ src/models/bert/interface.py
@@ -4,6 +4,7 @@ import logging
 import src.models.bert.architecture
 import src.elements.variable as vr
 import src.models.bert.settings
+import src.models.bert.parameters
 
 import ray.train.torch
 import ray.train
@@ -15,19 +16,27 @@ class Interface:
 
     def __init__(self, data: dict[str, ray.data.dataset.MaterializedDataset],
                  variable: vr.Variable, enumerator: dict):
+        """
+
+        :param data:
+        :param variable:
+        :param enumerator:
+        """
 
         self.__data = data
         self.__variable = variable
         self.__enumerator = enumerator
 
-        # Maximum steps per epoch
-        self.__max_steps_per_epoch: int = (
-                self.__variable.N_TRAIN // (self.__variable.TRAIN_BATCH_SIZE * self.__variable.N_GPU))
+        self.__parameters = src.models.bert.parameters.Parameters()
 
         # Additionally
         self.__settings = src.models.bert.settings.Settings(variable=self.__variable)
 
     def exc(self):
+        """
+
+        :return:
+        """
 
         arc = src.models.bert.architecture.Architecture()
 
@@ -37,13 +46,13 @@ class Interface:
             datasets={'train': self.__data['train'], 'eval': self.__data['validate']}
         )
 
+        # Tuner
         tuner = ray.tune.Tuner(
             trainable,
             param_space={
                 'train_loop_config': {
                     'learning_rate': ray.tune.grid_search([0.01, 0.02]),
                     'weight_decay': ray.tune.grid_search([0.1, 0.2]),
-                    'max_steps_per_epoch': self.__max_steps_per_epoch,
                     'variable': self.__variable,
                     'enumerator': self.__enumerator
                 },
@@ -54,6 +63,18 @@ class Interface:
                 metric='eval_loss', mode='min',
                 scheduler=self.__settings.scheduler(),
                 num_samples=2, reuse_actors=True
+            ),
+            run_config=ray.train.RunConfig(
+                name=self.__parameters.task,
+                storage_path=self.__parameters.storage_path,
+                progress_reporter=self.__settings.reporting(),
+                checkpoint_config=ray.train.CheckpointConfig(
+                    num_to_keep=5,
+                    checkpoint_score_attribute='eval_loss',
+                    checkpoint_score_order='min'
+                )
             )
 
         )
+
+        return tuner.fit()
