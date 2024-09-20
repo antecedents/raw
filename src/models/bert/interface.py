97bd295842277c92174bcabb9d60266e484d9879
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-20 16:25:42 +0100

    Update src/models/bert/interface.py

--- src/models/bert/interface.py
+++ src/models/bert/interface.py
@@ -30,12 +30,19 @@ class Interface:
         # From Hugging Face Trainer -> Ray Trainer
         trainable = ray.train.torch.TorchTrainer(
             arc.exc,
-            datasets={"train": self.__data["train"], "eval": self.__data["validate"]}
+            datasets={'train': self.__data['train'], 'eval': self.__data['validate']}
         )
 
         tuner = ray.tune.Tuner(
             trainable,
             param_space={
+                'train_loop_config': {
+                    'learning_rate': ray.tune.grid_search([0.01, 0.02]),
+                    'weight_decay': ray.tune.grid_search([0.1, 0.2]),
+                    'max_steps_per_epoch': self.__max_steps_per_epoch,
+                    'variable': self.__variable,
+                    'enumerator': self.__enumerator
+                },
                 'scaling_config': ray.train.ScalingConfig(
                     num_workers=self.__variable.N_GPU, use_gpu=True, trainer_resources={'CPU': self.__variable.N_CPU})
             }
