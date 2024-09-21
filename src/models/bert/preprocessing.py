cb5f475c59e623c4865fbcb41361440300670641
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-21 22:48:30 +0100

    Update src/models/bert/preprocessing.py

--- src/models/bert/preprocessing.py
+++ src/models/bert/preprocessing.py
@@ -1,7 +1,15 @@
+import transformers
+
+import src.models.bert.parameters
+import src.elements.variable as vr
+import src.models.bert.parameters
 
 class Preprocessing:
 
-    def __init__(self):
-        pass
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizerFast, variable: vr.Variable):
 
-    
+        self.__tokenizer = tokenizer
+        self.__variable = variable
+
+        # Additionally
+        self.__parameters = src.models.bert.parameters.Parameters()
