5d69248bb95596813624926d7fc0015663802b20
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-05 13:27:05 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 """Module main.py"""
+import datetime
 import logging
 import os
 import sys
-
-import ray
-import torch
 
 
 def main():
@@ -14,29 +12,7 @@ def main():
     """
 
     logger: logging.Logger = logging.getLogger(__name__)
-
-    # Device  Selection: Setting a graphics processing unit as the default device
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logger.info(msg=device)
-
-    # Ray
-    ray.init(dashboard_host='172.17.0.2', dashboard_port=8265)
-
-    # The raw data
-    data = src.data.source.Source().exc()
-    elements = src.data.tags.Tags(data=data).exc()
-    enumerator, archetype = src.data.encodings.Encodings().exc(elements=elements)
-
-    # Filtering
-    filtering = src.data.filtering.Filtering()
-    data = filtering(data=data, elements=elements)
-
-    # Structuring
-    data = src.data.structuring.Structuring(data=data).exc()
-
-    # Modelling
-    src.models.interface.Interface(
-        data=data, enumerator=enumerator, archetype=archetype).exc(architecture='bert')
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
 
 
 if __name__ == '__main__':
@@ -49,18 +25,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
-
-    # Activate graphics processing units
-    os.environ['CUDA_VISIBLE_DEVICES']='0'
-    os.environ['TOKENIZERS_PARALLELISM']='true'
-    os.environ['RAY_USAGE_STATS_ENABLED']='0'
-
-    # Classes
-    import src.data.encodings
-    import src.models.interface
-    import src.data.source
-    import src.data.filtering
-    import src.data.structuring
-    import src.data.tags
 
     main()
