b1de83079ca2734a85138fe85f3e9e7e15bf96c5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-05 23:08:02 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,21 @@ class Config:
 
     def __init__(self) -> None:
         """
-        Constructor
+        Constructor<br>
+        ------------<br>
+
+        Variables denoting a path - including or excluding a filename - have an underscore suffix; this suffix is
+        excluded for names such as warehouse, storage, depository, etc.<br><br>
         """
 
+        # Temporary local storage
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.parent_ = os.path.join(self.warehouse, 'data')
+        self.data_ = [os.path.join(self.parent_, 'raw', 'references'),
+                      os.path.join(self.parent_, 'raw', 'data'),
+                      os.path.join(self.parent_, 'latest', 'references'),
+                      os.path.join(self.parent_, 'latest', 'data')]
+
+        # Configuration files
+        self.s3_parameters_key = 's3_parameters.yaml'
+        self.locators = 'locators.yaml'
