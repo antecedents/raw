1f70c0df7df74f600741f9cd49e3f2414b3052af
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-06 17:07:21 +0000

    Update src/source/api.py

--- src/source/api.py
+++ src/source/api.py
@@ -1,15 +1,29 @@
-import urllib.request
+"""Module api.py"""
 import json
+import urllib.request
 
 import pandas as pd
 
+
 class API:
+    """
+    Notes<br>
+    ------<br>
+
+    For reading data sets from Scottish Health & Social Care <a href="https://www.opendata.nhs.scot" target="_blank">
+    Open Data Platform</a>
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
     def __get_dictionary(url: str):
+        """
+
+        :param url: An application programming interface endpoint
+        :return:
+        """
 
         try:
             blob = urllib.request.urlopen(url=url)
@@ -23,6 +37,11 @@ class API:
 
     @staticmethod
     def __data(dictionary: dict) -> pd.DataFrame:
+        """
+
+        :param dictionary: An API (application programming interface) query output.
+        :return:
+        """
 
         try:
             frame = pd.DataFrame.from_dict(data=dictionary['result']['records'], orient='columns')
@@ -32,6 +51,11 @@ class API:
         return frame
 
     def __call__(self, url: str) -> pd.DataFrame:
+        """
+
+        :param url: An application programming interface endpoint
+        :return:
+        """
 
         dictionary = self.__get_dictionary(url=url)
         data = self.__data(dictionary=dictionary)
