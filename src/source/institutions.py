417999b52b55ce26476e755d12fe8672d4091e23
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-06 02:22:54 +0000

    Update src/source/institutions.py

--- src/source/institutions.py
+++ src/source/institutions.py
@@ -26,3 +26,10 @@ class Institutions:
         self.__data = src.source.api.API().__call__(url=self.__url)
         self.__data.info()
         logging.info(self.__data)
+
+        self.__rename = {'HospitalCode': 'hospital_code', 'HospitalName': 'hospital_name',
+                         'AddressLine1': 'address_line_1', 'AddressLine2': 'address_line_2',
+                         'AddressLine3': 'address_line_3', 'AddressLine4': 'address_line_4',
+                         'PostCode': 'post_code', 'HealthBoard': 'health_board_code', 'HSCP': 'hscp_code',
+                         'CouncilArea': 'council_area', 'IntermediateZone': 'intermediate_zone',
+                         'DataZone': 'data_zone'}
