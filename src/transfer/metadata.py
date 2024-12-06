dc9e425bfc425e32214bf4b608d8d6f30a41de40
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-06 16:31:26 +0000

    Update src/transfer/metadata.py: In Progress

--- src/transfer/metadata.py
+++ src/transfer/metadata.py
@@ -1,19 +1,26 @@
 import os
 import logging
+import boto3
 
 import src.functions.objects
+import src.s3.configurations
 
 
 class Metadata:
 
-    def __init__(self):
+    def __init__(self, connector: boto3.session.Session):
+
+        self.__connector = connector
 
         self.__objects = src.functions.objects.Objects()
 
     def exc(self) -> dict:
 
-        uri = os.path.join(os.getcwd(), 'data', 'metadata.json')
-        dictionary = self.__objects.read(uri=uri)
+        # uri = os.path.join(os.getcwd(), 'data', 'metadata.json')
+        # dictionary = self.__objects.read(uri=uri)
+
+        dictionary = src.s3.configurations.Configurations(connector=self.__connector).objects(
+            key_name='raw/metadata.json')
         logging.info(dictionary)
 
         return dictionary
