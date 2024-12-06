008cbfae5af4eeaccac943c17122628b8997934b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-06 16:30:18 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ def main():
 
     # Hence
     src.source.interface.Interface(connector=connector).exc()
-    src.transfer.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
+    src.transfer.interface.Interface(connector=connector, service=service, s3_parameters=s3_parameters).exc()
 
     # Cache
     src.functions.cache.Cache().exc()
