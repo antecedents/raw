5239d7df820a5306dea4976e7012569e4178e8fa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-09 21:46:10 +0000

    Update config.py: In Progress

--- config.py
+++ config.py
@@ -18,10 +18,7 @@ class Config:
 
         # Temporary local storage
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
-        self.data_ = [os.path.join(self.warehouse, 'raw', 'references'),
-                      os.path.join(self.warehouse, 'raw', 'data'),
-                      os.path.join(self.warehouse, 'latest', 'references'),
-                      os.path.join(self.warehouse, 'latest', 'data')]
+        self.data_ = os.path.join(self.warehouse, 'data', 'raw')
 
         # Configuration files
         self.s3_parameters_key = 's3_parameters.yaml'
