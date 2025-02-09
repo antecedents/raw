08625d986073cf93ec4e16c5d8b4caa418f0b5db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-09 22:34:10 +0000

    Update src/source/inspect.py: In Progress

--- src/source/inspect.py
+++ src/source/inspect.py
@@ -14,4 +14,13 @@ class Inspect:
         assert tensor.shape[0] == 1, 'The # of distinct department types is > 1'
         assert np.all(np.char.equal(tensor, 'Type 1')), 'The department type of each instance must be Type 1'
 
+    def __country(self):
+
+        country_code = 'S92000003'
+
+        tensor = self.__data['Country'].unique()
+
+        assert tensor.shape[0] == 1, f'Error.  The data should be the data of a single country; {tensor.shape[0]} countries are present.'
+        assert np.all(np.char.equal(tensor, country_code)), f'Invalid country code.  The country code of each instance should be {country_code}'
+
 
