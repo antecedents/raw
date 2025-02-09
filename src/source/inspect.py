439c00b5157c7533f0855c20b16ebd5b6705ee5c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-09 22:40:48 +0000

    Update src/source/inspect.py: In Progress

--- src/source/inspect.py
+++ src/source/inspect.py
@@ -1,26 +1,33 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 class Inspect:
 
     def __init__(self, data: pd.DataFrame):
-
         self.__data = data
 
     def __department_type(self):
-
         tensor: np.ndarray = self.__data['DepartmentType'].unique()
 
         assert tensor.shape[0] == 1, 'The # of distinct department types is > 1'
         assert np.all(np.char.equal(tensor, 'Type 1')), 'The department type of each instance must be Type 1'
 
     def __country(self):
-
-        country_code = 'S92000003'
+        c_country = 'S92000003'
 
         tensor = self.__data['Country'].unique()
 
-        assert tensor.shape[0] == 1, f'Error.  The data should be the data of a single country; {tensor.shape[0]} countries are present.'
-        assert np.all(np.char.equal(tensor, country_code)), f'Invalid country code.  The country code of each instance should be {country_code}'
+        assert tensor.shape[0] == 1, \
+            f'Error.  The data should be the data of a single country; {tensor.shape[0]} countries are present.'
+        assert np.all(np.char.equal(tensor, c_country)), \
+            f'Invalid country code.  The country code of each instance should be {c_country}'
 
+    def __attendance_category(self):
+        c_attendance_category = 'Unplanned'
 
+        tensor: np.ndarray = self.__data['AttendanceCategory'].unique()
+
+        assert tensor.shape[0] == 1, 'The # of attendance categories is > 1; there should be 1, i.e., Unplanned, only.'
+        assert np.all(np.char.equal(tensor,
+                                    c_attendance_category)), f'Invalid attendance category.  The attendance category of each instance should be {c_attendance_category}'
