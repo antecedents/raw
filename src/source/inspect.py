f52e81fd32bc585ff96fb64678e44b0e13d88ba5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-09 22:26:51 +0000

    Update src/source/inspect.py: In Progress

--- src/source/inspect.py
+++ src/source/inspect.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import numpy as np
 
 class Inspect:
 
     def __init__(self, data: pd.DataFrame):
 
         self.__data = data
+
+    def __department_type(self):
+
+        tensor: np.ndarray = self.__data['DepartmentType'].unique()
+
+        assert tensor.shape[0] == 1, 'The # of distinct department types is > 1'
+        assert np.all(np.char.equal(tensor, 'Type 1')), 'The department type of each instance must be Type 1'
+
+
