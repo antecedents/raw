92cde3830f6b106d10fe6a7f15afd80f3406394c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-10 01:43:18 +0000

    Update src/functions/arguments.py

--- src/functions/arguments.py
+++ src/functions/arguments.py
@@ -1,14 +1,26 @@
+"""Module arguments.py"""
 import argparse
 
 class Arguments:
+    """
+    Notes<br>
+    ------<br>
+
+    Processes the project's input arguments.
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
     def restart(value):
+        """
 
-        if not str(value) == 'restart':
+        :param value:
+        :return:
+        """
+
+        if str(value) != 'restart':
             raise argparse.ArgumentTypeError('The only valid value of this optional argument is restart')
 
         return True
