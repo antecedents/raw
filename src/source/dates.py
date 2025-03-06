62e75ac85a786dcd21458112bc42de902a2fb9f6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-06 13:36:41 +0000

    Update src/source/dates.py

--- src/source/dates.py
+++ src/source/dates.py
@@ -22,6 +22,20 @@ class Dates:
         self.__data = data
 
     @dask.delayed
+    def __get_data(self, code: str) -> pd.DataFrame:
+        """
+
+        :param code:
+        :return:
+        """
+
+        excerpt = self.__data.copy().loc[self.__data['hospital_code'] == code, :]
+        excerpt = excerpt.copy().groupby(by=['week_ending_date', 'health_board_code', 'hospital_code']).sum()
+        excerpt.reset_index(drop=False, inplace=True)
+
+        return excerpt
+
+    @dask.delayed
     def __indices(self, blob: pd.DataFrame) -> pd.DataFrame:
         """
 
@@ -37,20 +51,6 @@ class Dates:
         indices.rename(columns={0: 'week_ending_date'}, inplace=True)
 
         return indices
-
-    @dask.delayed
-    def __get_data(self, code: str) -> pd.DataFrame:
-        """
-
-        :param code:
-        :return:
-        """
-
-        excerpt = self.__data.copy().loc[self.__data['hospital_code'] == code, :]
-        excerpt = excerpt.copy().groupby(by=['week_ending_date', 'health_board_code', 'hospital_code']).sum()
-        excerpt.reset_index(drop=False, inplace=True)
-
-        return excerpt
 
     @dask.delayed
     def __dates(self, indices: pd.DataFrame, blob: pd.DataFrame) -> pd.DataFrame:
